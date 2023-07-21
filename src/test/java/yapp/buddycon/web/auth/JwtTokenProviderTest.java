f55d52a18a3f4cdf8e715ad91e6acfc7eb3a23da
yubin <dbqls2821@gmail.com>
2023-07-21 18:37:01 +0900

    test: TokenProvider가 refresh token을 한번 저장하는가에 대한 테스트 코드 작성

--- src/test/java/yapp/buddycon/web/auth/JwtTokenProviderTest.java
+++ src/test/java/yapp/buddycon/web/auth/JwtTokenProviderTest.java
@@ -2,7 +2,7 @@ package yapp.buddycon.web.auth;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Date;
@@ -11,11 +11,14 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class JwtTokenProviderTest {
+  TokenCreator tokenCreator = Mockito.mock(TokenCreator.class);
+  Time time = Mockito.mock(Time.class);
+  CacheStorage cacheStorage = Mockito.mock(CacheStorage.class);
+  JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(cacheStorage, tokenCreator, time);
 
   @Test
-  void 토큰을_생성할때_token_creator는_한번_invocation_된다(@Mock TokenCreator tokenCreator, @Mock Time time, @Mock CacheStorage cacheStorage) {
+  void 토큰을_생성할때_token_creator는_한번_invocation_된다() {
     // given
-    JwtTokenProvider jwtTokenProvider = new JwtTokenProvider(cacheStorage, tokenCreator, time);
     User user = new User(1L, 12345678L);
     Date testTime = new Date();
 
@@ -27,5 +30,21 @@ class JwtTokenProviderTest {
 
     // then
     verify(tokenCreator, times(1)).createToken(user, testTime, testTime, testTime);
+  }
+
+  @Test
+  void 토큰을_생성할때_refresh_token을_한번_저장한다() {
+    // given
+    User user = new User(1L, 12345678L);
+    Date testTime = new Date();
+
+    when(time.getNow()).thenReturn(testTime);
+    when(tokenCreator.createToken(user, testTime, testTime, testTime)).thenReturn(new Token("access", "refresh"));
+
+    // when
+    jwtTokenProvider.provide(user);
+
+    // then
+    verify(cacheStorage, times(1)).saveWithExpiration(anyString(), anyString(), anyLong());
   }
 }
