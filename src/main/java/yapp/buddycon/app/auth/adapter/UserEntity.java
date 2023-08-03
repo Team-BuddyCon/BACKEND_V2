e6434afdfe378980efb941f9fcb1a337871b4465
yubin <dbqls2821@gmail.com>
2023-08-03 17:54:34 +0900

    feat: adapter영역에 UserEntity 추가

---
+++ src/main/java/yapp/buddycon/app/auth/adapter/UserEntity.java
@@ -0,0 +1,20 @@
+package yapp.buddycon.app.auth.adapter;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private Long clientId;
+}
