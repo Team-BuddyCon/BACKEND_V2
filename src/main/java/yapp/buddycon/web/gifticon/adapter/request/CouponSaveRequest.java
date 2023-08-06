e8aa86ad89c5f492f608de3b3b127c9d86f0a58d
huey-J <wkdgus7113@gmail.com>
2023-08-06 16:38:13 +0900

    BE-23 사용하지 않는 코드 주석 처리

---
+++ src/main/java/yapp/buddycon/web/gifticon/adapter/request/CouponSaveRequest.java
@@ -0,0 +1,9 @@
+package yapp.buddycon.web.gifticon.adapter.request;
+
+//import yapp.buddycon.web.gifticon.domain.code.Store;
+//import java.time.LocalDate;
+
+public record CouponSaveRequest(
+//  String name, String barcode, LocalDate expireTime, String imageUrl, Store store, String memo
+) {
+}
