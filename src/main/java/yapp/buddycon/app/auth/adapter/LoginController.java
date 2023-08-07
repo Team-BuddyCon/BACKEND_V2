c5ce98069dfa278239d9311f28c2b9af022464dd
yubin <dbqls2821@gmail.com>
2023-08-07 15:17:03 +0900

    feat: 로그인 controller request값 validation 추가

--- src/main/java/yapp/buddycon/app/auth/adapter/LoginController.java
+++ src/main/java/yapp/buddycon/app/auth/adapter/LoginController.java
@@ -1,5 +1,6 @@
 package yapp.buddycon.app.auth.adapter;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -18,7 +19,7 @@ public class LoginController {
     private final Login login;
 
     @PostMapping("login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest request) {
+    public ResponseEntity<?> login(@RequestBody @Valid LoginRequest request) {
         Token token = login.login(request.accessToken());
         return ApiResponse.success("로그인에 성공하였습니다.", token);
     }
