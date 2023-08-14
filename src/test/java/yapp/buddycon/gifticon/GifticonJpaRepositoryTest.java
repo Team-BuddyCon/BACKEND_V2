1f3bd82ec24fdf4b99eb215a20528f55376e0e02
huey-J <wkdgus7113@gmail.com>
2023-08-15 00:34:19 +0900

    Refactor GifticonJpaRepositoryTest 테스트 메소드 분리

--- src/test/java/yapp/buddycon/gifticon/GifticonJpaRepositoryTest.java
+++ src/test/java/yapp/buddycon/gifticon/GifticonJpaRepositoryTest.java
@@ -3,6 +3,7 @@ package yapp.buddycon.gifticon;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.time.LocalDate;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -10,7 +11,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import yapp.buddycon.web.gifticon.adapter.request.SearchGifticonDTO;
@@ -34,28 +34,21 @@ public class GifticonJpaRepositoryTest {
   class findAll {
 
     @Test
-    void 정상조회() {
+    void 일반목록조회() {
       // given
-      Gifticon gifticon1 = createGifticon("name1", false, null);
-      Gifticon gifticon2 = createGifticon("name2", true, null);
+      createGifticon("name1", false, null);
+      createGifticon("name2", true, null);
 
       // when
-      SearchGifticonDTO dto1 = new SearchGifticonDTO();
-      Page<GifticonResponseDTO> result1 = gifticonJpaRepository.findAll(dto1, PageRequest.of(0, 10));
-
-      SearchGifticonDTO dto2 = new SearchGifticonDTO();
-      dto2.setUsed(false);
-      Page<GifticonResponseDTO> result2 = gifticonJpaRepository.findAll(dto2, PageRequest.of(0, 10));
+      Page<GifticonResponseDTO> result = gifticonJpaRepository.findAll(
+          new SearchGifticonDTO(), PageRequest.of(0, 10));
 
       // then
-      assertThat(result1.getTotalElements()).isEqualTo(2);
-
-      assertThat(result2.getTotalElements()).isEqualTo(1);
-      assertThat(result2.getContent().get(0).getName()).isEqualTo("name1");
+      assertThat(result.getTotalElements()).isEqualTo(2);
     }
 
     @Test
-    void 빈리스트() {
+    void 빈목록조회() {
       // given
 
       // when
@@ -68,99 +61,116 @@ public class GifticonJpaRepositoryTest {
     @Test
     void 정렬조회_이름() {
       // given
-      Gifticon gifticon1 = createGifticon("name1", true, null);
-      Gifticon gifticon3 = createGifticon("name3", true, null);
-      Gifticon gifticon2 = createGifticon("name2", true, null);
+      createGifticon("name1", true, null);
+      Gifticon 조회대상 = createGifticon("name3", true, null);
+      createGifticon("name2", true, null);
 
       // when
-      SearchGifticonDTO dto1 = new SearchGifticonDTO();
-      dto1.setSortType(SearchGifticonSortType.NAME);
-      dto1.setSortDirection(Direction.ASC);
-      Page<GifticonResponseDTO> result1 = gifticonJpaRepository.findAll(dto1, PageRequest.of(0, 10));
-
-      SearchGifticonDTO dto2 = new SearchGifticonDTO();
-      dto2.setSortType(SearchGifticonSortType.NAME);
-      Page<GifticonResponseDTO> result2 = gifticonJpaRepository.findAll(dto2, PageRequest.of(0, 10));
+      SearchGifticonDTO dto = new SearchGifticonDTO();
+      dto.setSortType(SearchGifticonSortType.NAME);
+      Page<GifticonResponseDTO> result = gifticonJpaRepository.findAll(dto, PageRequest.of(0, 10));
 
       // then
-      assertThat(result1.getTotalElements()).isEqualTo(3);
-      assertThat(result1.getContent().get(0).getName()).isEqualTo(gifticon1.getName());
-
-      assertThat(result2.getTotalElements()).isEqualTo(3);
-      assertThat(result2.getContent().get(0).getName()).isEqualTo(gifticon3.getName());
+      assertThat(result.getTotalElements()).isEqualTo(3);
+      assertThat(result.getContent().get(0).getName()).isEqualTo(조회대상.getName());
     }
 
-    @Test
-    void 필터링조회() {
-      // given
-      Gifticon gifticon1 = createGifticon("gifticon1", true, null);
+    @Nested
+    class 필터링조회 {
+      private StoreCategory 카테고리1, 카테고리2;
+      private Store 카테고리1_가맹점1, 카테고리1_가멍잼2, 카테고리2_가맹점3;
+      private Gifticon 가맹점_없는_기프티콘1, 카테고리1_가맹점1_기프티콘2, 카테고리1_가맹점2_기프티콘3, 카테고리2_가맹점3_기프티콘4, 카테고리2_가맹점3_기프티콘5_미사용;
 
-      StoreCategory storeCategory1 = createStoreCategory("category1");
-      Store store1 = createStore(1l, "store1", storeCategory1);
-      Gifticon gifticon2 = createGifticon("gifticon2", true, store1);
-      Store store2 = createStore(2l, "store2", storeCategory1);
-      Gifticon gifticon3 = createGifticon("gifticon3", true, store2);
+      @BeforeEach
+      void initData() {
+        가맹점_없는_기프티콘1 = createGifticon("gifticon1", true, null);
 
-      StoreCategory storeCategory2 = createStoreCategory("category2");
-      Store store3 = createStore(3l, "store3", storeCategory2);
-      Gifticon gifticon4 = createGifticon("gifticon4", true, store3);
-      Gifticon gifticon5 = createGifticon("gifticon4", false, store3);
+        카테고리1 = createStoreCategory("category1");
+        카테고리1_가맹점1 = createStore("store1", 카테고리1);
+        카테고리1_가맹점1_기프티콘2 = createGifticon("gifticon2", true, 카테고리1_가맹점1);
+        카테고리1_가멍잼2 = createStore("store2", 카테고리1);
+        카테고리1_가맹점2_기프티콘3 = createGifticon("gifticon3", true, 카테고리1_가멍잼2);
 
-      // when
-      SearchGifticonDTO dto1 = new SearchGifticonDTO();
-      Page<GifticonResponseDTO> result1 = gifticonJpaRepository.findAll(dto1, PageRequest.of(0, 10));
+        카테고리2 = createStoreCategory("category2");
+        카테고리2_가맹점3 = createStore("store3", 카테고리2);
+        카테고리2_가맹점3_기프티콘4 = createGifticon("gifticon4", true, 카테고리2_가맹점3);
+        카테고리2_가맹점3_기프티콘5_미사용 = createGifticon("gifticon4", false, 카테고리2_가맹점3);
+      }
 
-      SearchGifticonDTO dto2 = new SearchGifticonDTO();
-      dto2.setStoreId(2l);
-      Page<GifticonResponseDTO> result2 = gifticonJpaRepository.findAll(dto2, PageRequest.of(0, 10));
+      @Test
+      void 필터링없음() {
+        // given
+        SearchGifticonDTO dto = new SearchGifticonDTO();
 
-      SearchGifticonDTO dto3 = new SearchGifticonDTO();
-      dto3.setStoreCategoryId(storeCategory2.getId());
-      Page<GifticonResponseDTO> result3 = gifticonJpaRepository.findAll(dto3, PageRequest.of(0, 10));
+        // when
+        Page<GifticonResponseDTO> result = gifticonJpaRepository.findAll(dto, PageRequest.of(0, 10));
 
-      // then
-      assertThat(result1.getTotalElements()).isEqualTo(5);
+        // then
+        assertThat(result.getTotalElements()).isEqualTo(5);
+      }
 
-      assertThat(result2.getTotalElements()).isEqualTo(1);
-      assertThat(result2.getContent().get(0).getStoreName()).isEqualTo(store2.getName());
+      @Test
+      void 사용여부_필터링() {
+        // given
+        SearchGifticonDTO dto = new SearchGifticonDTO();
+        dto.setUsed(false);
 
-      assertThat(result3.getTotalElements()).isEqualTo(2);
-      assertThat(result3.getContent().get(0).getStoreCategoryName()).isEqualTo(storeCategory2.getName());
+        // when
+        Page<GifticonResponseDTO> result = gifticonJpaRepository.findAll(dto, PageRequest.of(0, 10));
+
+        // then
+        assertThat(result.getTotalElements()).isEqualTo(1);
+      }
+
+      @Test
+      void 카테고리_가맹점_필터링() {
+        // given
+        SearchGifticonDTO dto = new SearchGifticonDTO();
+        dto.setStoreCategoryId(카테고리1.getId());
+        dto.setStoreId(카테고리1_가멍잼2.getId());
+
+        // when
+        Page<GifticonResponseDTO> result = gifticonJpaRepository.findAll(dto, PageRequest.of(0, 10));
+
+        // then
+        assertThat(result.getTotalElements()).isEqualTo(1);
+        assertThat(result.getContent().get(0).getGifticonId()).isEqualTo(카테고리1_가맹점2_기프티콘3.getId());
+      }
     }
 
-    // TODO Entity 메소드로 분리 예정
-    /** Create Start **/
-    private Gifticon createGifticon(String name, boolean used, Store store) {
-      Gifticon gifticon = Gifticon.builder()
-          .barcode("aaaa")
-          .imageUrl("url1")
-          .name(name)
-          .expireDate(LocalDate.now())
-          .used(used)
-          .store(store)
-          .build();
-      return gifticonJpaRepository.save(gifticon);
-    }
-
-    private Store createStore(Long id, String name, StoreCategory storeCategory) {
-      Store store = Store.builder()
-          .id(id)
-          .name(name)
-          .storeCategory(storeCategory)
-          .build();
-
-      jpaRepository.save(store);
-      return store;
-    }
-
-    private StoreCategory createStoreCategory(String name) {
-      StoreCategory storeCategory = StoreCategory.builder()
-          .name(name)
-          .build();
-      jpaRepository.save(storeCategory);
-      return storeCategory;
-    }
-    /** Create End **/
   }
+
+  // TODO Entity 메소드로 분리 예정
+  /** Create Start **/
+  private Gifticon createGifticon(String name, boolean used, Store store) {
+    Gifticon gifticon = Gifticon.builder()
+        .barcode("aaaa")
+        .imageUrl("url1")
+        .name(name)
+        .expireDate(LocalDate.now())
+        .used(used)
+        .store(store)
+        .build();
+    return gifticonJpaRepository.save(gifticon);
+  }
+
+  private Store createStore(String name, StoreCategory storeCategory) {
+    Store store = Store.builder()
+        .name(name)
+        .storeCategory(storeCategory)
+        .build();
+
+    jpaRepository.save(store);
+    return store;
+  }
+
+  private StoreCategory createStoreCategory(String name) {
+    StoreCategory storeCategory = StoreCategory.builder()
+        .name(name)
+        .build();
+    jpaRepository.save(storeCategory);
+    return storeCategory;
+  }
+  /** Create End **/
 
 }
