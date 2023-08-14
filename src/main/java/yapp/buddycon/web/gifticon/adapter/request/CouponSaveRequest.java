0a109630e51094f6718eac6c58658169bcf10f93
huey-J <wkdgus7113@gmail.com>
2023-08-14 13:48:58 +0900

    Delete 사용하지 않는 클래스 삭제

--- src/main/java/yapp/buddycon/web/gifticon/adapter/request/CouponSaveRequest.java
+++
@@ -1,9 +0,0 @@
-package yapp.buddycon.web.gifticon.adapter.request;
-
-//import yapp.buddycon.web.gifticon.domain.code.Store;
-//import java.time.LocalDate;
-
-public record CouponSaveRequest(
-//  String name, String barcode, LocalDate expireTime, String imageUrl, Store store, String memo
-) {
-}
