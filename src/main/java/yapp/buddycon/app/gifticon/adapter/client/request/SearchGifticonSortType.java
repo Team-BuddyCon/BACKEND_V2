2cbbf2dc2de5b0126bea1c70766b781c5c3875ea
huey-J <wkdgus7113@gmail.com>
2023-08-25 02:43:52 +0900

    [Refactor] SearchGifticonSortType sort 형식 변경

--- src/main/java/yapp/buddycon/app/gifticon/adapter/client/request/SearchGifticonSortType.java
+++ src/main/java/yapp/buddycon/app/gifticon/adapter/client/request/SearchGifticonSortType.java
@@ -3,14 +3,18 @@ package yapp.buddycon.app.gifticon.adapter.client.request;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.data.domain.Sort;
+import yapp.buddycon.app.gifticon.adapter.infra.entity.GifticonEntity;
 
 @Getter
 @AllArgsConstructor
 public enum SearchGifticonSortType {
 
-  EXPIRE_DATE("유효 기간 순", Sort.by("expireDate").descending()),
-  CREATED_AT("생성 일시 순", Sort.by("createdAt").descending()),
-  NAME("이름 순", Sort.by("name").ascending()),
+  EXPIRE_DATE("유효 기간 순",
+      Sort.sort(GifticonEntity.class).by(GifticonEntity::getExpireDate).descending()),
+  CREATED_AT("생성 일시 순",
+      Sort.sort(GifticonEntity.class).by(GifticonEntity::getCreatedAt).descending()),
+  NAME("이름 순",
+      Sort.sort(GifticonEntity.class).by(GifticonEntity::getName).ascending()),
   ;
 
   private String description;
