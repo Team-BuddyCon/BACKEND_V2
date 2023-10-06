38972886a3ab98a69f50b6c31480190b86712a11
yubin <dbqls2821@gmail.com>
2023-10-06 22:31:57 +0900

    feat: AuthUser가 필요한 통합테스트에서 사용할 Mock ArgumentResolver 추가

---
+++ src/test/java/yapp/buddycon/app/common/MockAuthenticationArgumentResolver.java
@@ -0,0 +1,24 @@
+package yapp.buddycon.app.common;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.ModelAndViewContainer;
+import yapp.buddycon.app.auth.adapter.AuthenticationArgumentResolver;
+import yapp.buddycon.app.auth.adapter.jwt.JwtTokenDecryptor;
+import yapp.buddycon.common.AuthUser;
+
+public class MockAuthenticationArgumentResolver extends AuthenticationArgumentResolver {
+
+    public MockAuthenticationArgumentResolver() {
+        super(new JwtTokenDecryptor());
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter methodParameter,
+                                  ModelAndViewContainer modelAndViewContainer,
+                                  NativeWebRequest nativeWebRequest,
+                                  WebDataBinderFactory webDataBinderFactory) {
+        return new AuthUser(1L);
+    }
+}
