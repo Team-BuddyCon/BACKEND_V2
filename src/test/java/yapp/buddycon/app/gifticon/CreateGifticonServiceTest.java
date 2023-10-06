42cc395ef178a926e63b9dbbb308408e8380f1ad
yubin <dbqls2821@gmail.com>
2023-10-06 22:56:03 +0900

    test: service 테스트 코드 수정

--- src/test/java/yapp/buddycon/app/gifticon/CreateGifticonServiceTest.java
+++ src/test/java/yapp/buddycon/app/gifticon/CreateGifticonServiceTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import yapp.buddycon.app.gifticon.adapter.client.request.GifticonCreationDto;
 import yapp.buddycon.app.gifticon.application.port.out.GifticonCommandStorage;
@@ -20,8 +21,7 @@ import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class CreateGifticonServiceTest {
@@ -30,24 +30,27 @@ class CreateGifticonServiceTest {
     GifticonCommandStorage gifticonCommandStorage;
 
     @Mock
-    UserQueryStorage userQueryStorage;
-
-    @Mock
     ImageUploader imageUploader;
 
     @InjectMocks
     CreateGifticonService service;
 
     @Test
-    void 정상적으로_기프티콘을_저장한다() throws IOException {
+    void 정상적으로_기프티콘을_저장한다() {
 
         // given
-        final var dto = new GifticonCreationDto("name", LocalDate.now(), GifticonStore.GS25, null);
+        final var dto = new GifticonCreationDto("name", LocalDate.MAX, GifticonStore.GS25, null);
         final var userId = 1L;
-        final var image = new MockMultipartFile("test-gifticon-img.png", new FileInputStream("src/test/resources/img/test-gifticon-img.png"));
+        final var testImage = new MockMultipartFile(
+                "image",
+                "image.png",
+                MediaType.IMAGE_PNG_VALUE,
+                "testpng".getBytes()
+        );
+        when(imageUploader.upload(testImage)).thenReturn("s3 image url");
 
         // when
-        service.createGifticon(dto, image, userId);
+        service.createGifticon(dto, testImage, userId);
 
         // then
         verify(gifticonCommandStorage, times(1)).save(any());
