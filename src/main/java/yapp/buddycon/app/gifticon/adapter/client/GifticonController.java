f9c518adc5e3e75053136c82a6b2ce08472b5eaf
huey-J <wkdgus7113@gmail.com>
2023-10-16 22:20:10 +0900

    feat: 기프티콘 목록 조회 시 ApiResponse로 반환하도록 수정

--- src/main/java/yapp/buddycon/app/gifticon/adapter/client/GifticonController.java
+++ src/main/java/yapp/buddycon/app/gifticon/adapter/client/GifticonController.java
@@ -3,10 +3,12 @@ package yapp.buddycon.app.gifticon.adapter.client;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Slice;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import yapp.buddycon.app.common.response.ApiResponse;
 import yapp.buddycon.app.gifticon.adapter.client.request.SearchAvailableGifticonDTO;
 import yapp.buddycon.app.gifticon.adapter.client.response.GifticonResponseDTO;
 import yapp.buddycon.app.gifticon.application.port.in.GifticonUseCase;
@@ -33,9 +35,10 @@ public class GifticonController {
   }
 
   @GetMapping("/{gifticon-id}")
-  public GifticonResponseDTO getGifticon(
-      AuthUser authUser, @PathVariable("gifticon-id") long gidticonId) {
-    return gifticonUseCase.getGifticon(authUser.id(), gidticonId);
+  public ResponseEntity<?> getGifticon(
+      AuthUser authUser, @PathVariable("gifticon-id") long gifticonId) {
+    return ApiResponse.success("기프티콘 목록을 성공적으로 조회하였습니다.",
+        gifticonUseCase.getGifticon(authUser.id(), gifticonId));
   }
 
 }
