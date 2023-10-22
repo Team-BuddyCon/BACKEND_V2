44062e6bed4596851bf7197ecb395744785e90f8
yubin <dbqls2821@gmail.com>
2023-10-23 01:49:16 +0900

    feat: authorization header 생성

--- src/main/java/yapp/buddycon/config/SwaggerConfig.java
+++ src/main/java/yapp/buddycon/config/SwaggerConfig.java
@@ -3,6 +3,8 @@ package yapp.buddycon.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,9 +12,15 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openAPI() {
-        return new OpenAPI()
-                .components(new Components())
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("Bearer Authentication"))
+                .components(new Components().addSecuritySchemes("Bearer Authentication", createAPIKeyScheme()))
                 .info(apiInfo());
+    }
+
+    private SecurityScheme createAPIKeyScheme() {
+        return new SecurityScheme().type(SecurityScheme.Type.HTTP)
+                .bearerFormat("JWT")
+                .scheme("bearer");
     }
 
     private Info apiInfo() {
