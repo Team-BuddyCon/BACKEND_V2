326719cdb0ade53f713a5a8f6de832a034a25a98
yubin <dbqls2821@gmail.com>
2023-10-23 01:49:37 +0900

    feat: Controller에 swagger 어노테이션 적용

--- src/main/java/yapp/buddycon/app/auth/adapter/client/LoginController.java
+++ src/main/java/yapp/buddycon/app/auth/adapter/client/LoginController.java
@@ -1,5 +1,7 @@
 package yapp.buddycon.app.auth.adapter.client;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -9,8 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import yapp.buddycon.app.auth.application.service.AuthService;
 import yapp.buddycon.app.auth.application.service.TokenDto;
-import yapp.buddycon.app.common.response.ApiResponse;
+import yapp.buddycon.app.common.response.ResponseEntityApi;
 
+@Tag(name = "로그인", description = "로그인 API")
 @RestController
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
@@ -18,10 +21,11 @@ public class LoginController {
 
     private final AuthService authService;
 
+    @Operation(summary = "로그인")
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody @Valid LoginRequest request) {
         TokenDto tokenDto = authService.login(request);
-        return ApiResponse.successWithBody("로그인에 성공하였습니다.", tokenDto);
+        return ResponseEntityApi.successWithBody("로그인에 성공하였습니다.", tokenDto);
     }
 
 }
