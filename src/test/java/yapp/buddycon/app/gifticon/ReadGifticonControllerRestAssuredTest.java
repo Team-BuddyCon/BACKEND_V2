fcea5a3b963bd5a265d34a227f57dc7c5834a713
huey-J <wkdgus7113@gmail.com>
2023-11-01 01:42:33 +0900

    test: AuthenticationArgumentResolver 모킹하도록 수정
    Co-authored-by: yubin <dbqls2821@gmail.com>

--- src/test/java/yapp/buddycon/app/gifticon/ReadGifticonControllerRestAssuredTest.java
+++ src/test/java/yapp/buddycon/app/gifticon/ReadGifticonControllerRestAssuredTest.java
@@ -2,24 +2,23 @@ package yapp.buddycon.app.gifticon;
 
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import yapp.buddycon.app.auth.adapter.jwt.JwtTokenProvider;
-import yapp.buddycon.app.auth.application.service.TokenDto;
+import yapp.buddycon.app.auth.adapter.AuthenticationArgumentResolver;
+import yapp.buddycon.app.common.AuthUser;
 import yapp.buddycon.app.gifticon.adapter.GifticonException;
 import yapp.buddycon.app.gifticon.adapter.GifticonException.GifticonExceptionCode;
 import yapp.buddycon.app.gifticon.adapter.client.response.GifticonResponseDTO;
 import yapp.buddycon.app.gifticon.application.port.in.ReadGifticonUseCase;
-import yapp.buddycon.app.user.domain.User;
 
 import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
@@ -31,19 +30,17 @@ public class ReadGifticonControllerRestAssuredTest {
     @MockBean
     private ReadGifticonUseCase gifticonUseCase;
 
-    @Autowired
-    private JwtTokenProvider provider;
-
-    private String ACCESS_TOKEN;
+    @MockBean
+    private AuthenticationArgumentResolver argumentResolver;
 
     @LocalServerPort
     int port;
 
     @BeforeEach
-    void setUp() {
+    void setUp() throws Exception {
+        when(argumentResolver.resolveArgument(any(), any(), any(), any())).thenReturn(new AuthUser(1L));
+        when(argumentResolver.supportsParameter(any())).thenReturn(true);
         RestAssured.port = port;
-        TokenDto tokenDto = provider.provide(new User(1000L, 1L, "test", "test", "test", "test"));
-        ACCESS_TOKEN = "Bearer " + tokenDto.accessToken();
     }
 
     @Nested
@@ -62,12 +59,13 @@ public class ReadGifticonControllerRestAssuredTest {
                     .log().all()
                     .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
-                    .header("Authorization", ACCESS_TOKEN)
+                    .header("Authorization", "abc")
                     .get("/api/v1/gifticons/" + "1");
 
             // then
             response
                 .then().log().all()
+                .body("body.gifticonId", Matchers.is(gifticonResponseDTO.gifticonId().intValue()))
                 .statusCode(HttpStatus.OK.value())
                 .extract();
         }
@@ -84,7 +82,7 @@ public class ReadGifticonControllerRestAssuredTest {
                     .log().all()
                     .contentType(MediaType.APPLICATION_JSON_VALUE)
                 .when()
-                    .header("Authorization", ACCESS_TOKEN)
+                    .header("Authorization", "abc")
                     .get("/api/v1/gifticons/" + "1");
 
             // then
