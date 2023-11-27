6063e4e47fb4fb93265359758ecaf512458fcfe7
huey-J <wkdgus7113@gmail.com>
2023-11-28 00:42:28 +0900

    feat: SignUpDecider에서 NotificationSettingCreationEvent 발행하도록 수정

--- src/main/java/yapp/buddycon/app/auth/application/service/SignUpDecider.java
+++ src/main/java/yapp/buddycon/app/auth/application/service/SignUpDecider.java
@@ -1,9 +1,12 @@
 package yapp.buddycon.app.auth.application.service;
 
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 import yapp.buddycon.app.auth.adapter.client.LoginRequest;
 import yapp.buddycon.app.auth.application.port.out.OAuthUserInfoApi;
+import yapp.buddycon.app.event.NotificationSettingCreationEvent;
 import yapp.buddycon.app.user.application.port.out.UserCommandStorage;
 import yapp.buddycon.app.user.application.port.out.UserQueryStorage;
 import yapp.buddycon.app.user.domain.User;
@@ -15,11 +18,23 @@ public class SignUpDecider {
   private final UserQueryStorage userQueryStorage;
   private final UserCommandStorage userCommandStorage;
   private final OAuthUserInfoApi oAuthUserInfoApi;
+  private final ApplicationEventPublisher applicationEventPublisher;
 
   public User decide(LoginRequest request) {
     OAuthMemberInfo memberInfo = oAuthUserInfoApi.call(request.oauthAccessToken());
     Long clientId = memberInfo.id();
-    return userQueryStorage.findByClientId(clientId).orElseGet(()
-            -> userCommandStorage.save(new User(null, clientId, request.nickname(), request.email(), request.gender(), request.age())));
+    Optional<User> byClientId = userQueryStorage.findByClientId(clientId);
+
+    if (byClientId.isPresent()) {
+      return byClientId.get();
+    }
+
+    User createdUser = userCommandStorage.save(
+        new User(null, clientId, request.nickname(), request.email(), request.gender(),
+            request.age()));
+
+    applicationEventPublisher.publishEvent(new NotificationSettingCreationEvent(createdUser.id()));
+
+    return createdUser;
   }
 }
