d07ec1c2e4e01971a89c40a830db4e675bb088cc
huey-J <wkdgus7113@gmail.com>
2024-01-17 01:41:34 +0900

    feat: 공지사항 상세 조회 Repository 추가

--- src/main/java/yapp/buddycon/app/notification/adapter/infra/JpaAnnouncementNotiQueryStorage.java
+++ src/main/java/yapp/buddycon/app/notification/adapter/infra/JpaAnnouncementNotiQueryStorage.java
@@ -2,16 +2,22 @@ package yapp.buddycon.app.notification.adapter.infra;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import yapp.buddycon.app.notification.adapter.AnnouncementNotiException;
+import yapp.buddycon.app.notification.adapter.AnnouncementNotiException.AnnouncementNotiExceptionCode;
 import yapp.buddycon.app.notification.adapter.client.response.AnnouncementNotiResponseDTO;
+import yapp.buddycon.app.notification.adapter.infra.jpa.AnnouncementNotiJpaRepository;
 import yapp.buddycon.app.notification.application.port.out.AnnouncementNotiQueryStorage;
 
 @RequiredArgsConstructor
 @Component
 public class JpaAnnouncementNotiQueryStorage implements AnnouncementNotiQueryStorage {
 
+  private final AnnouncementNotiJpaRepository announcementNotiJpaRepository;
+
   @Override
   public AnnouncementNotiResponseDTO getById(Long id) {
-    // TODO implement
-    return null;
+    return announcementNotiJpaRepository.findByIdAsResponseDTO(id)
+        .orElseThrow(() -> new AnnouncementNotiException(
+            AnnouncementNotiExceptionCode.ANNOUNCEMENT_NOTI_NOT_FOUND));
   }
 }
