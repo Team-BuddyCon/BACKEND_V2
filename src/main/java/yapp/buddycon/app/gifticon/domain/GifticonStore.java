d0d9d62bdbd59acbf3426c50c68d73c1a2087ff7
huey-J <wkdgus7113@gmail.com>
2024-02-05 23:20:46 +0900

    feat: GifticonStore Enum 새로운 값 추가

--- src/main/java/yapp/buddycon/app/gifticon/domain/GifticonStore.java
+++ src/main/java/yapp/buddycon/app/gifticon/domain/GifticonStore.java
@@ -10,6 +10,10 @@ public enum GifticonStore {
   STARBUCKS("스타벅스", GifticonStoreCategory.CAFE),
   TWOSOME_PLACE("투썸 플레이스", GifticonStoreCategory.CAFE),
   GONG_CHA("공차", GifticonStoreCategory.CAFE),
+  HOLLYS("할리스", GifticonStoreCategory.CAFE),
+  ANGELINUS("엔제리너스", GifticonStoreCategory.CAFE),
+  COFFEE_BEAN("커피빈", GifticonStoreCategory.CAFE),
+  MEGA_COFFEE("메가커피", GifticonStoreCategory.CAFE),
 
   GS25("지에스 25", GifticonStoreCategory.CONVENIENCE_STORE),
   CU("씨유", GifticonStoreCategory.CONVENIENCE_STORE),
