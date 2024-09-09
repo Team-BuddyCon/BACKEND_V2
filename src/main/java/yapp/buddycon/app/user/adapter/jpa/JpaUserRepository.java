dcc7ea2c6f6b8a91dce86c00ccffc781ad610e51
Huey-J <wkdgus7113@gmail.com>
2024-09-09 17:59:13 +0900

    feat: User 조회 시 native query 로 조회할 수 있도록 수정

--- src/main/java/yapp/buddycon/app/user/adapter/jpa/JpaUserRepository.java
+++ src/main/java/yapp/buddycon/app/user/adapter/jpa/JpaUserRepository.java
@@ -3,9 +3,12 @@ package yapp.buddycon.app.user.adapter.jpa;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
 
 public interface JpaUserRepository extends JpaRepository<UserEntity, Long> {
     boolean existsByClientId(Long clientId);
 
+
+    @Query(value = "SELECT * FROM users u WHERE u.client_id = :clientId AND u.deleted = false", nativeQuery = true)
     Optional<UserEntity> findByClientIdAndDeletedIsFalse(Long clientId);
 }
